d0e47f1dc30b74fcab829b75c331c9bf5181b7d1
LOL 228 <nursultanr585@gmail.com>
2022-09-08 17:42:50 +0600

    a

---
+++ react-js-gane/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+
+import { useState } from 'react'
+// import {classNames} from 'react'
+function App() {
+
+  const enemy = document.querySelector('.enemy')
+  const [isActive, setIsActive] = useState(false)
+  // const characterClass = className({
+  //   'character'
+  // });
+  const click = () => {
+    // if(character)
+    console.log()
+
+    if (document.querySelector('.character').classList == 'character') {
+      document.querySelector('.character').classList.add('jumping-character')
+      console.log(document.querySelector('.character'))
+
+    } 
+    setTimeout(() => {
+      document.querySelector('.character').classList.remove('jumping-character')
+    }, 1000);
+  }
+
+    // setInterval(() => { 
+
+    // const characterTop = parseInt(getComputedStyle(character).getPropertyValue('top')); 
+
+    // const characterTop = parseInt(window.load.getComputedStyle(character).getPropertyValue('top')); 
+
+    // const enemyLeft = parseInt(window.load.getComputedStyle(enemy).getPropertyValue('left')); 
+
+    // const enemyLeft = parseInt(getComputedStyle(enemy).getPropertyValue('left'))
+
+    // if (enemyLeft >= 0 && enemyLeft < 20 && characterTop > 118) { 
+    // enemy.style.animation = 'none'; 
+    // enemy.style.display = 'none'; 
+    // alert('You lost!'); 
+    // } 
+    // }, 100)
+
+    return (
+      <div className='container' onClick={click}>
+        {/* <div className='character' className={isActive ? 'character' : 'jumping-character'}></div> */}
+        <div className='character'></div>
+        <div className='enemy'></div>
+      </div>
+    )
+  }
+
+  export default App;
